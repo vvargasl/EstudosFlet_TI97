afd6ad66c665ee5248973f398176756bb9e52b38
Larissa Vargas Pimentel <larissavargaspimentel@gmail.com>
2024-06-27 12:07:26 -0300

    Card
    UPDATE

--- Comecando_Telas/Elementos/layout.py
+++ Comecando_Telas/Elementos/layout.py
@@ -4,21 +4,23 @@ def main(page:Page):
     page.title="Estudando Elementos"
     produto=Card(
         content=Container(
+            bgcolor=colors.AMBER_100,
+            width=200,
+
             content=Column(
                 controls=[
-                    Image(src="burg.png"),
-                    Text("Lanche Stake"),
-                    Text("Lanche Feito"),
+                    Row(controls=[Image(src="burg.png")],alignment=MainAxisAlignment.CENTER),
+                    Row(controls=[Text("Stake de Frango")],alignment=MainAxisAlignment.CENTER),
+                    Row(controls=[Text("Lanche Feito",max_lines=2)],
+                        alignment=MainAxisAlignment.CENTER),
                     Row(controls=[
-                        Icon(cupertino_icons.HEART),
-                        Icon(icons.PAYMENT)
-                    ]
-
-                    )
+                        Icon(cupertino_icons.HEART,color=colors.RED),
+                        Icon(icons.MONEY,color=colors.GREEN)
+                    ],alignment=MainAxisAlignment.CENTER)
 
                 ]
+            ),
 
-            )
 
         )
 
